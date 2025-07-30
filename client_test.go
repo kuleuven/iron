1c926b372c3b6d66989556bc1739124d1752ff70
Peter Verraedt <peter@verraedt.be>
2025-07-30 15:51:05 +0200

    Increase waittime
    Signed-off-by: Peter Verraedt <peter@verraedt.be>

--- client_test.go
+++ client_test.go
@@ -358,7 +358,7 @@ func TestClientUpload(t *testing.T) { //nolint:funlen
 
 		transfer.BufferSize = 100
 		transfer.MinimumRangeSize = 200
-		transfer.CopyBufferDelay = 100 * time.Millisecond
+		transfer.CopyBufferDelay = 500 * time.Millisecond
 
 		return client.Upload(context.Background(), f.Name(), "test", transfer.Options{
 			//	SyncModTime: true,
